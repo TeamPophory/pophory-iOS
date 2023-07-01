d0392431b7b69ec614d655e7c8b15426aa314155
Joon Baek <joonbaek12@gmail.com>
2023-07-01 10:56:40 +0900

    [Setting] #9 - FontLiterals 수정

--- pophory-iOS/Global/Consts/FontLiterals.swift
+++ pophory-iOS/Global/Consts/FontLiterals.swift
@@ -13,27 +13,31 @@ extension UIFont {
     }
     
     @nonobjc class var h2: UIFont {
-        return UIFont.font(.pretendardSemiBold, ofSize: 22)
-    }
-    
-    @nonobjc class var h3: UIFont {
         return UIFont.font(.pretendardSemiBold, ofSize: 20)
     }
     
-    @nonobjc class var t1: UIFont {
+    @nonobjc class var h3: UIFont {
         return UIFont.font(.pretendardSemiBold, ofSize: 18)
+    }
+    
+    @nonobjc class var ttl1: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 17)
+    }
+    
+    @nonobjc class var t1: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 16)
     }
     
     @nonobjc class var t2: UIFont {
         return UIFont.font(.pretendardMedium, ofSize: 18)
     }
     
-    @nonobjc class var b1: UIFont {
-        return UIFont.font(.pretendardRegular, ofSize: 16)
+    @nonobjc class var c1: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 14)
     }
     
-    @nonobjc class var tg1: UIFont {
-        return UIFont.font(.pretendardRegular, ofSize: 14)
+    @nonobjc class var c2: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 12)
     }
 }
 
