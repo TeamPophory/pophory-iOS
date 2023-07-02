5fa027d2c6b765877b676fd4acdad5703d4f1d6e
Hongjunehuke <junhyeok2166@daum.net>
2023-07-02 18:45:52 +0900

    [ADD] ImageLiterals 파일 추가

---
+++ pophory-iOS/Global/Consts/ImageLiterals.swift
@@ -0,0 +1,33 @@
+//
+//  ImageLiterals.swift
+//  pophory-iOS
+//
+//  Created by 홍준혁 on 2023/07/02.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    
+    //MARK: - tab bar icon
+    
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+    static func load(systemName: String) -> UIImage {
+        guard let image = UIImage(systemName: systemName, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = systemName
+        return image
+    }
+}
+
