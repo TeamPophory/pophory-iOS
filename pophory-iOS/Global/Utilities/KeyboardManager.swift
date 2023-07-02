dd522937b25a30346ca0c977e6e9a8faaa5446f1
Joon Baek <joonbaek12@gmail.com>
2023-07-03 02:26:58 +0900

    [Feat] #20 - KeyboardManager, Keyboard-Following Button 기능 구현

---
+++ pophory-iOS/Global/Utilities/KeyboardManager.swift
@@ -0,0 +1,46 @@
+//
+//  KeyboardManager.swift
+//  pophory-iOS
+//
+//  Created by Joon Baek on 2023/07/03.
+//
+
+import UIKit
+
+class KeyboardManager: NSObject {
+    weak var bottomConstraint: NSLayoutConstraint?
+    weak var viewController: UIViewController?
+
+    init(bottomConstraint: NSLayoutConstraint?, viewController: UIViewController?) {
+        self.bottomConstraint = bottomConstraint
+        self.viewController = viewController
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            
+            let buttonBottomConstraints = 18
+            
+            let keyboardHeight: CGFloat
+            keyboardHeight = keyboardSize.height - (self.viewController?.view.safeAreaInsets.bottom ?? 0)
+            
+            self.bottomConstraint?.constant = -1 * keyboardHeight - CGFloat(buttonBottomConstraints)
+            self.viewController?.view.layoutIfNeeded()
+        }
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        self.bottomConstraint?.constant = 0
+        self.viewController?.view.layoutIfNeeded()
+    }
+
+    func keyboardAddObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func keyboardRemoveObserver() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+}
--- pophory-iOS/Screen/Auth/NameInputViewController.swift
+++ pophory-iOS/Screen/Auth/NameInputViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class NameInputViewController: BaseViewController, Navigatable {
     
+    // MARK: - UI Components
+    
     var navigationBarTitleText: String? { return "회원가입" }
+    
+    var bottomConstraint: NSLayoutConstraint?
+    
+    var keyboardManager: KeyboardManager!
     
     lazy var nameInputView: NameInputView = {
         let view = NameInputView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    // MARK: - Life Cycle
     
     override func loadView() {
         super.loadView()
@@ -26,9 +34,34 @@ class NameInputViewController: BaseViewController, Navigatable {
     
     override func viewWillAppear(_ animated: Bool) {
         setupNavigationBar(with: PophoryNavigationConfigurator.shared)
+        keyboardManager = KeyboardManager(bottomConstraint: bottomConstraint, viewController: self)
+               keyboardManager.keyboardAddObserver()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        self.bottomConstraint = NSLayoutConstraint(item: nameInputView.nextButton, attribute: .bottom, relatedBy: .equal, toItem: safeArea, attribute: .bottom, multiplier: 1.0, constant: 0)
+        self.bottomConstraint?.isActive = true
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        keyboardManager.keyboardRemoveObserver()
+    }
+    
+    deinit {
+        keyboardManager.keyboardRemoveObserver()
+        keyboardManager = nil
+    }
+}
+
+extension NameInputViewController {
+    
+    // MARK: - objc
+    
+    // MARK: - Private Functions
+    
 }
