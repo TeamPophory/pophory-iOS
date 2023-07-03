c95830cd165bfa05d8d1620168f94b9901dd2ab1
Joon Baek <joonbaek12@gmail.com>
2023-07-03 10:28:25 +0900

    [Feat] #20 - UITextField 작성정책 반영 기능구현

--- pophory-iOS/Screen/Auth/Views/NameInputView.swift
+++ pophory-iOS/Screen/Auth/Views/NameInputView.swift
@@ -16,7 +16,7 @@ class NameInputView: UIView {
         label.textColor = .black
         label.font = .h1
         label.numberOfLines = 0
-        label.asColor(targetString: "너의 이름", color: .pophoryPurple)
+        label.applyColorToString(targetString: "너의 이름", color: .pophoryPurple)
         //TODO: LineHeight적용
         return label
     }()
@@ -30,20 +30,25 @@ class NameInputView: UIView {
     }()
     
     let bodyLabel: UILabel = {
+        let normalFont = UIFont.t2
+        let boldFont = UIFont.h3
+
         let label = UILabel()
         label.text = "포포리 사용을 위해 실명 입력이 필요해요"
         label.textColor = .pophoryGray500
-        //TODO: fontsize
-        label.font = .t2
+        label.applyBoldTextTo("실명 입력", withFont: normalFont, boldFont: boldFont)
         return label
     }()
+
     
     let inputTextField: UITextField = {
         let textField = UITextField()
-        textField.borderStyle = .roundedRect
         textField.placeholder = "이름(성+이름)"
         textField.textColor = .black
         textField.font = .t1
+        textField.layer.borderColor = UIColor.pophoryGray400.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 18
         textField.addPadding(left: 15)
         return textField
     }()
@@ -53,6 +58,15 @@ class NameInputView: UIView {
         label.font = .t1
         label.textColor = .pophoryGray400
         label.text = "(0/6)"
+        return label
+    }()
+    
+    let warningLabel: UILabel = {
+        let label = UILabel()
+        label.font = .c1
+        label.textColor = .pophoryRed
+        label.text = "*6글자를 초과했습니다"
+        label.isHidden = true
         return label
     }()
     
@@ -66,6 +80,7 @@ class NameInputView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         inputTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        setupDelegate()
         setupViews()
     }
     
@@ -74,12 +89,14 @@ class NameInputView: UIView {
     }
 }
 
+// MARK: - Extensions
+
 extension NameInputView {
     
     // MARK: - Layout
     
     private func setupViews() {
-        addSubviews([headerLabel, bodyStackView, charCountLabel, nextButton])
+        addSubviews([headerLabel, bodyStackView, charCountLabel, nextButton, warningLabel])
         bodyStackView.addArrangedSubviews([bodyLabel, inputTextField])
         
         headerLabel.snp.makeConstraints {
@@ -106,6 +123,11 @@ extension NameInputView {
             $0.trailing.equalTo(inputTextField)
         }
         
+        warningLabel.snp.makeConstraints {
+            $0.top.equalTo(charCountLabel)
+            $0.leading.equalToSuperview().offset(26)
+        }
+        
         nextButton.addCenterXConstraint(to: self)
     }
     
@@ -113,15 +135,46 @@ extension NameInputView {
     
     // MARK: - Private Methods
     
+    private func setupDelegate() {
+        inputTextField.delegate = self
+    }
+    
     func updateCharCountLabel(charCount: Int) {
         charCountLabel.text = "(\(charCount)/6)"
     }
 }
 
+// MARK: - UITextFieldDelegate
+
 extension NameInputView: UITextFieldDelegate {
     
+    @objc func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text?.count == 0 {
+            textField.layer.borderColor = UIColor.pophoryPurple.cgColor
+        }
+    }
+    
+    @objc func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.pophoryGray400.cgColor
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.count + string.count - range.length
+
+        if newLength > 6 {
+            textField.layer.borderColor = UIColor.pophoryRed.cgColor
+            warningLabel.isHidden = false
+            return false
+        } else {
+            textField.textColor = .black
+            textField.layer.borderColor = UIColor.pophoryPurple.cgColor
+            warningLabel.isHidden = true
+        }
+        return true
+    }
+
     @objc func textDidChange(_ textField: UITextField) {
-        guard let text = textField.text else { return }
-        updateCharCountLabel(charCount: text.count)
+        updateCharCountLabel(charCount: textField.text?.count ?? 0)
     }
 }
