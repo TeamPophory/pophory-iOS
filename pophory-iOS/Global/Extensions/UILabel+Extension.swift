644d08f1d2678ea8516ba6d804ba7e24ea136584
Joon Baek <joonbaek12@gmail.com>
2023-07-03 10:29:26 +0900

    [Add] #20 - UILabel+에 applyColorToString, applyBoldTextTo 추가

--- pophory-iOS/Global/Extensions/UILabel+Extension.swift
+++ pophory-iOS/Global/Extensions/UILabel+Extension.swift
@@ -19,4 +19,22 @@ extension UILabel {
             self.attributedText = attributedStr
         }
     }
+    
+    /// 선택된 텍스트의 색상 조정
+    func applyColorToString(targetString: String, color: UIColor) {
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttribute(.foregroundColor, value: color, range: range)
+        attributedText = attributedString
+    }
+    
+    /// 선택된 텍스트 볼드처리
+    func applyBoldTextTo(_ partOfString: String, withFont font: UIFont, boldFont: UIFont) {
+        let normalText = self.text ?? ""
+        let attributedString = NSMutableAttributedString(string: normalText, attributes: [NSAttributedString.Key.font: font])
+        let boldRange = (normalText as NSString).range(of: partOfString)
+        attributedString.setAttributes([NSAttributedString.Key.font: boldFont], range: boldRange)
+        self.attributedText = attributedString
+    }
 }
