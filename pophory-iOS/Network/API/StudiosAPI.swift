c06abb2f906505cb48fcc5f3ca15a53ce87e8148
Hongjunehuke <junhyeok2166@daum.net>
2023-07-03 17:11:03 +0900

    [Add] Network API 구현

---
+++ pophory-iOS/Network/API/StudiosAPI.swift
@@ -0,0 +1,37 @@
+//
+//  StudiosAPI.swift
+//  pophory-iOS
+//
+//  Created by 홍준혁 on 2023/07/03.
+//
+
+import Foundation
+
+import Moya
+
+enum StudiosAPI {
+    case patchStudios
+}
+
+extension StudiosAPI: BaseTargetType {
+    var path: String {
+        switch self {
+        case .patchStudios:
+            return URLConstants.studio
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .patchStudios:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .patchStudios:
+            return .requestPlain
+        }
+    }
+}
