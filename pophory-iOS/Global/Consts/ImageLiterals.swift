9d1772e8e4ab832ae6f60ecaf98dae3ce759c4cf
Daye Kim <dykim315@naver.com>
2023-07-05 03:04:58 +0900

    [Chore] #36 ImageLiterals 수정

--- pophory-iOS/Global/Consts/ImageLiterals.swift
+++ pophory-iOS/Global/Consts/ImageLiterals.swift
@@ -9,14 +9,24 @@ import UIKit
 
 enum ImageLiterals {
     
-    //MARK: - tab bar icon
+    // MARK: - tab bar icon
     
     static var tabBarHomeAlbumIcon: UIImage { .load(name: "tabBarHomeAlbumIcon") }
     static var tabBarEditAlbumIcon: UIImage { .load(name: "tabBarEditAlbumIcon") }
     static var tabBarMyPageIcon: UIImage { .load(name: "tabBarMyPageIcon") }
     
+    //MARK: - logo icon
+    
     static var logIcon: UIImage { .load(name: "logIcon") }
+    
+    // MARK: - icon
+    
+    static var searchIcon: UIImage { .load(name: "searchIcon") }
+    static var downIcon: UIImage { .load(name: "downIcon") }
+    static var calanderIcon: UIImage { .load(name: "calanderIcon") }
+    
 }
+
 
 extension UIImage {
     static func load(name: String) -> UIImage {
