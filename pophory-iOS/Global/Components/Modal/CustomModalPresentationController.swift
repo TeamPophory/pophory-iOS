ce56367257dc40c8e52a86bf98e840eb694c9b29
Daye Kim <dykim315@naver.com>
2023-07-06 03:11:19 +0900

    [Chore] #34 컨벤션 준수를 위한 코드 수정

--- pophory-iOS/Global/Components/Modal/CustomModalPresentationController.swift
+++ pophory-iOS/Global/Components/Modal/CustomModalPresentationController.swift
@@ -9,21 +9,72 @@ import UIKit
 
 class CustomModalPresentationController: UIPresentationController {
     
-    let customHeight: CGFloat
+    // MARK: - Properties
+
+    private let customHeight: CGFloat
+    
+    // MARK: - UI Properties
+    
+    private let blurEffectView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .pophoryBlack
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    init(presentedViewController: UIViewController, presenting
+         presentingViewController: UIViewController?, customHeight: CGFloat) {
+        self.customHeight = customHeight
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+        
+        setupTapGesture()
+    }
+    
+    // MARK: - override
     
     override var frameOfPresentedViewInContainerView: CGRect {
-        guard let containerView = containerView else {
-            return CGRect.zero
-        }
-
+        guard let containerView = containerView else { return CGRect.zero }
         let safeAreaInsets = containerView.safeAreaInsets
         let height = customHeight + safeAreaInsets.top + safeAreaInsets.bottom
         
         return CGRect(x: 0, y: containerView.bounds.height - height, width: containerView.bounds.width, height: height)
     }
     
-    init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?, customHeight: CGFloat) {
-        self.customHeight = customHeight
-        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+    override func presentationTransitionWillBegin() {
+        guard let containerView = containerView else { return }
+        
+        blurEffectView.alpha = 0
+        containerView.addSubview(blurEffectView)
+        presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0.3},completion: nil)
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0}, completion: { _ in self.blurEffectView.removeFromSuperview()})
+    }
+
+    // 모달의 크기가 조절됐을 때 호출
+    override func containerViewDidLayoutSubviews() {
+        guard let containerView = containerView else { return }
+
+        super.containerViewDidLayoutSubviews()
+        blurEffectView.frame = containerView.bounds
+    }
+}
+
+extension CustomModalPresentationController {
+    
+    // MARK: - @objc
+    
+    @objc func dismissController() {
+        self.presentedViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Private Methods
+
+    private func setupTapGesture() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissController))
+        blurEffectView.addGestureRecognizer(tapGestureRecognizer)
     }
 }
--- pophory-iOS/Global/Components/Modal/CustomModalTransitionDelegate.swift
+++ pophory-iOS/Global/Components/Modal/CustomModalTransitionDelegate.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class CustomModalTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
-    let customHeight: CGFloat
     
+    // MARK: - Properties
+
+    private let customHeight: CGFloat
+    
+    // MARK: - Life Cycle
+
     init(customHeight: CGFloat) {
         self.customHeight = customHeight
         super.init()
     }
+    
+    // MARK: - func
     
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         let presentationController = CustomModalPresentationController(presentedViewController: presented, presenting: presenting, customHeight: customHeight)
