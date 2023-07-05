bf07bbfc90d56132e131a7389abf9851b64d1497
Hongjunehuke <junhyeok2166@daum.net>
2023-07-05 14:21:05 +0900

    [Chore] SceneDelegte root 뷰 변경

--- pophory-iOS/Global/Resources/SceneDelegate.swift
+++ pophory-iOS/Global/Resources/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 let window = UIWindow(windowScene: windowScene)
                 window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
                 
-                let rootVC = AlbumDetailViewController()
+                let rootVC = TabBarController()
                 let navigationController = UINavigationController(rootViewController: rootVC)
                 
                 window.rootViewController = navigationController
