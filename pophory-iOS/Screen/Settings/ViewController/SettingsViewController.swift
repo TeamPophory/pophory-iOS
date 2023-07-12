49de91d3290e4921d61b6047f394c3826a815687
Danna <danna.wa@kakaobrain.com>
2023-07-12 21:07:32 +0900

    [Feat] #111 - 공지사항, 개인정보처리방침, 이용약관 웹뷰 연결

--- pophory-iOS/Screen/Settings/ViewController/SettingsViewController.swift
+++ pophory-iOS/Screen/Settings/ViewController/SettingsViewController.swift
@@ -19,6 +19,7 @@ class SettingsViewController: BaseViewController {
         super.loadView()
         
         view = rootView
+        rootView.delegate = self
     }
 
     override func viewDidLoad() {
@@ -36,4 +37,32 @@ class SettingsViewController: BaseViewController {
 
 extension SettingsViewController: Navigatable {
     var navigationBarTitleText: String? { "설정" }
+}
+
+// MARK: -
+
+extension SettingsViewController: SettingsRootViewDelegate {
+    func handleOnClickNotice() {
+        let vc = PophoryWebViewController(urlString: "https://pophoryofficial.wixsite.com/pophory/notice", title: "공지사항")
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func handleOnClickPrivacyPolicy() {
+        let vc = PophoryWebViewController(urlString: "https://pophoryofficial.wixsite.com/pophory/gaeinjeongbo-ceoribangcim/%E2%80%8B%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4-%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8", title: "개인정보 처리방침")
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func handleOnClickTerms() {
+        let vc = PophoryWebViewController(urlString: "https://pophoryofficial.wixsite.com/pophory/copy-of-gaeinjeongbo-ceoribangcim/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80", title: "이용약관")
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func handleOnClickLogOut() {
+        return
+    }
+    
+    func handleOnClickDeleteAccount() {
+        return
+    }
+    
 }
