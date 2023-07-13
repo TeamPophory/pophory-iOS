9e0f805ac3874bf0d2ae27e50b645c7d7b5f8bfa
Joon Baek <joonbaek12@gmail.com>
2023-07-13 23:21:24 +0900

    [Add] #64 - 소셜 로그인 및 중복아이디 관련 AuthAPI 추가

--- pophory-iOS/Network/API/AuthAPI.swift
+++ pophory-iOS/Network/API/AuthAPI.swift
@@ -10,19 +10,26 @@ import Foundation
 import Moya
 
 enum AuthAPI {
+    case appleLogin(authorizationCode: String)
+    case sendIdentityToken(identityToken: String)
+    case checkDuplicateNickname(nickname: String)
     case withdrawUser
 }
 
 extension AuthAPI: BaseTargetType {
     var path: String {
         switch self {
-        case .withdrawUser:
+        case .appleLogin, .sendIdentityToken, .withdrawUser:
             return URLConstants.auth
+        case .checkDuplicateNickname:
+            return URLConstants.memeber
         }
     }
     
     var method: Moya.Method {
         switch self {
+        case .appleLogin, .sendIdentityToken, .checkDuplicateNickname:
+            return .post
         case .withdrawUser:
             return .delete
         }
@@ -30,6 +37,13 @@ extension AuthAPI: BaseTargetType {
     
     var task: Moya.Task {
         switch self {
+        case .appleLogin(let authorizationCode):
+            return .requestParameters(parameters: ["authorizationCode" : authorizationCode], encoding: JSONEncoding.default)
+        case .sendIdentityToken(let identityToken):
+            return .requestParameters(parameters: ["identityToken": identityToken], encoding: JSONEncoding.default)
+        case .checkDuplicateNickname(let nickname):
+            let parameters: [String: Any] = ["nickname": nickname]
+            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
         case .withdrawUser:
             return .requestPlain
         }
