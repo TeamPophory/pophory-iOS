50cd17183465d3d9c419cf6b93815001c440a9ae
Daye Kim <dykim315@naver.com>
2023-07-18 00:15:57 +0900

    [Feat] #115 사진 삭제 API 연결

--- pophory-iOS/Screen/PhotoDetail/ViewController/PhotoDetailViewController.swift
+++ pophory-iOS/Screen/PhotoDetail/ViewController/PhotoDetailViewController.swift
@@ -11,6 +11,7 @@ final class PhotoDetailViewController: BaseViewController {
     
     // MARK: - Properties
     
+    private var photoID: Int?
     private var image: String?
     private var takenAt: String?
     private var studio: String?
@@ -31,7 +32,26 @@ final class PhotoDetailViewController: BaseViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         view = photoDetailView
+    }
+}
+
+extension PhotoDetailViewController {
+    
+    @objc func rightButtonOnClick() {
+        if let photoID = photoID {
+            requestDeletePhoto(photoId: photoID)
+        }
+    }
+    
+    func setData(photoID: Int, imageUrl: String, takenAt: String, studio: String, type: PhotoCellType) {
+        self.photoID = photoID
+        self.image = imageUrl
+        self.takenAt = takenAt
+        self.studio = studio
+        self.photoType = type
     }
 }
 
@@ -42,10 +62,17 @@ extension PhotoDetailViewController: Navigatable {
 }
 
 extension PhotoDetailViewController {
-    func setData(imageUrl: String, takenAt: String, studio: String, type: PhotoCellType) {
-        self.image = imageUrl
-        self.takenAt = takenAt
-        self.studio = studio
-        self.photoType = type
+    func requestDeletePhoto(
+        photoId: Int
+    ) {
+        NetworkService.shared.photoRepository.deletePhoto(
+            photoId: photoId
+        ) { result in
+            switch result {
+            case .success(let response):
+                self.navigationController?.popViewController(animated: true)
+            default : return
+            }
+        }
     }
 }
