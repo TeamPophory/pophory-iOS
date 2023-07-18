02853b5371d7a4e76397bcefeabded481441d4ee
Daye Kim <dykim315@naver.com>
2023-07-19 00:34:13 +0900

    [Feat] #149 Presigned Url 이미지 업로드 구현
    코드 수정 필요

--- pophory-iOS/Screen/AddPhoto/ViewController/AddPhotoViewController.swift
+++ pophory-iOS/Screen/AddPhoto/ViewController/AddPhotoViewController.swift
@@ -108,11 +108,17 @@ extension AddPhotoViewController {
                           primaryText: "포포리 앨범이 가득찼어요",
                           secondaryText: "아쉽지만,\n다음 업데이트에서 만나요!", firstButtonHandler: goToHome)
             } else {
-                guard let multipartData = fetchMultiPartData() else { return }
-                requestPostPhotoAPI(photoInfo: multipartData)
+//                guard let multipartData = fetchMultiPartData() else { return }
+//                requestPostPhotoAPI(photoInfo: multipartData)
+                if let urlString = presignedURL?.presignedUrl, let url = URL(string: urlString) {
+                        uploadImageToPresignedURL(image: photoImage, presignedURL: url, completion: {_ in
+                        })
+                    } else {
+                        print("Invalid URL")
+                    }
+                }
             }
         }
-    }
     
     // MARK: - Private Methods
     
@@ -225,4 +231,32 @@ extension AddPhotoViewController {
             }
         })
     }
+    
+    func uploadImageToPresignedURL(image: UIImage, presignedURL: URL, completion: @escaping (Error?) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+            completion(NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to convert image to data"]))
+            return
+        }
+        
+        var request = URLRequest(url: presignedURL)
+        request.httpMethod = "PUT"
+        request.setValue("image/jpeg", forHTTPHeaderField: "Content-Type")
+        request.httpBody = imageData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            // Check the response status code to ensure successful upload
+            if let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) {
+                completion(nil)  // Upload successful
+                print("업로드 성공")
+            }
+        }
+        task.resume()
+    }
+
 }
