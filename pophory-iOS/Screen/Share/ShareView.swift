d27d56df69028e1c31276e74df084353b2103d31
Daye Kim <dykim315@naver.com>
2023-07-19 21:40:48 +0900

    [Add] #160 초기 파일 추가

---
+++ pophory-iOS/Screen/Share/ShareView.swift
@@ -0,0 +1,20 @@
+//
+//  ShareView.swift
+//  pophory-iOS
+//
+//  Created by 김다예 on 2023/07/19.
+//
+
+import UIKit
+
+class ShareView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ pophory-iOS/Screen/Share/ShareViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShareViewController.swift
+//  pophory-iOS
+//
+//  Created by 김다예 on 2023/07/19.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
