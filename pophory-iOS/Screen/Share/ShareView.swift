8a9f3ad5cc71d56322024e459ca0301977020721
Daye Kim <dykim315@naver.com>
2023-07-19 21:41:43 +0900

    [Feat] #160 공유 수락하기 뷰 구현 - image만

--- pophory-iOS/Screen/Share/ShareView.swift
+++ pophory-iOS/Screen/Share/ShareView.swift
@@ -8,13 +8,70 @@
 import UIKit
 
 class ShareView: UIView {
+    
+    // MARK: - UI Properties
+    
+    private let sharePhotoView: UIView = {
+        let view = UIView()
+        view.makeRounded(radius: 20)
+        view.layer.borderColor = UIColor.pophoryGray400.cgColor
+        view.layer.borderWidth = 1
+        view.layer.shadowOffset = .init(width: 8, height: 8)
+        return view
+    }()
+    
+    let shareButton: PophoryButton = {
+    let buttonBuilder = PophoryButtonBuilder()
+    .setStyle(.primaryBlack)
+    .setTitle(.share)
+    return buttonBuilder.build()
+    }()
+    
+    let shareImageView = UIImageView()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension ShareView {
+    
+    private func setupStyle() {
+        self.backgroundColor = .pophoryWhite
+    }
+    
+    // MARK: - Layout
+    
+    private func setupLayout() {
+        self.addSubviews([shareButton, sharePhotoView])
+        
+        sharePhotoView.addSubviews([shareImageView])
+        
+        sharePhotoView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(34)
+            $0.bottom.equalTo(shareButton.snp.top).offset(-43)
+        }
+        
+        shareButton.addCenterXConstraint(to: self)
+        shareButton.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(10)
+        }
+        
+        shareImageView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(88)
+            $0.bottom.equalToSuperview().inset(59)
+        }
+    }
+    
 }
--- pophory-iOS/Screen/Share/ShareViewController.swift
+++ pophory-iOS/Screen/Share/ShareViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class ShareViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - UI Properties
+    
+    private let rootView = ShareView()
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = rootView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideNavigationBar()
     }
-    */
-
 }
