53780e2f9684161f2f7826bb6712e77925e87993
Hongjunehuke <junhyeok2166@daum.net>
2023-07-20 04:05:06 +0900

    [Add] 네트워크 환경 예외처리

---
+++ pophory-iOS/Global/Utilities/NetworkMonitor.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkMonitor.swift
+//  pophory-iOS
+//
+//  Created by 홍준혁 on 2023/07/20.
+//
+
+import Foundation
+
+import Network
+
+final class NetworkMonitor {
+    
+    private let queue = DispatchQueue.global(qos: .background)
+    private let monitor: NWPathMonitor
+    
+    init() {
+        monitor = NWPathMonitor()
+        dump(monitor)
+        print("------------")
+    }
+    
+    func startMonitoring(statusUpdateHandler: @escaping (NWPath.Status) -> Void) {
+        monitor.pathUpdateHandler = { path in
+            statusUpdateHandler(path.status)
+        }
+        monitor.start(queue: queue)
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
---
+++ pophory-iOS/Global/Utilities/NoNetworkView.swift
@@ -0,0 +1,43 @@
+//
+//  ServerFailViewController.swift
+//  pophory-iOS
+//
+//  Created by 홍준혁 on 2023/07/20.
+//
+
+import UIKit
+
+import SnapKit
+
+final class NoNetworkView: UIView {
+    
+    private let networkfailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.networkFail
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func render() {
+        self.addSubview(networkfailImageView)
+    
+        networkfailImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(248)
+            $0.width.equalTo(196)
+        }
+    }
+}
