cd114203399c3e8054f5df3a038bff9e97a3cdce
Hongjunehuke <junhyeok2166@daum.net>
2023-07-19 14:13:29 +0900

    [Add] 앨범 사진 Full Alert show

--- pophory-iOS/Screen/HomeAlbum/HomeAlbumViewController.swift
+++ pophory-iOS/Screen/HomeAlbum/HomeAlbumViewController.swift
@@ -22,6 +22,7 @@ final class HomeAlbumViewController: BaseViewController {
                     let photoCount: Int = albums[0].photoCount ?? 0
                     
                     // MARK: - update UI
+                    
                     homeAlbumView.albumImageView.image = ImageLiterals.albumCoverList[albumCover]
                     homeAlbumView.statusLabelText = String(photoCount)
                     
@@ -29,6 +30,16 @@ final class HomeAlbumViewController: BaseViewController {
                     homeAlbumView.updateProgressBarWidth(updateWidth: progressValue)
                     let isAlbumFull = (photoCount == 15) ? true : false
                     homeAlbumView.updateProgressBarIcon(isAlbumFull: isAlbumFull)
+                    
+                    // MARK: - alert
+                    
+                    if isAlbumFull {
+                        showPopup(
+                            image : ImageLiterals.img_albumfull,
+                            primaryText: "포포리 앨범이 가득찼어요",
+                            secondaryText: "아쉬지만,\n다음 업데이트에서 만나요!"
+                        )
+                    }
                 }
             }
         }
