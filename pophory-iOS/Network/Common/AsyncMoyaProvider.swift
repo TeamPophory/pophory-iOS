d73b2200ebf492ff677e7fa0fc655b710ac2d846
joonBaek12 <joonbaek12@syuin.ac.kr>
2023-09-02 21:11:44 +0900

    [Delete] #210 - available(iOS 15.0) 관련 코드 삭제

--- pophory-iOS/Network/Common/AsyncMoyaProvider.swift
+++ pophory-iOS/Network/Common/AsyncMoyaProvider.swift
@@ -8,7 +8,6 @@
 import Moya
 
 class AsyncMoyaProvider<Target: TargetType>: MoyaProvider<Target> {
-    @available(iOS 15.0, *)
     func request(_ target: Target) async throws -> Response {
         return try await withCheckedThrowingContinuation { continuation in
             self.request(target) { result in
