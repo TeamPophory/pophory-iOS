b406497b9da5c04a03cff42d335b952f98cba1ef
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-10-02 02:47:24 +0900

    [Chore] #229 - Sentry_dsn info.plist에 추가

--- pophory-iOS/Global/Resources/AppDelegate.swift
+++ pophory-iOS/Global/Resources/AppDelegate.swift
@@ -22,11 +22,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // MARK: - Sentry SDK 관련
         SentrySDK.start { options in
-            options.dsn = ""
-            options.debug = true // Enabled debug when first installing is always helpful
-            
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production.
+            if let dsn = Bundle.main.infoDictionary?["SENTRY_DSN"] as? String {
+                options.dsn = dsn
+            }
+            options.debug = true
             options.tracesSampleRate = 1.0
         }
         
@@ -54,5 +53,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UIInterfaceOrientationMask.portrait
     }
     
+    
+//    func showCustomTrackingAuthorizationDialog() {
+//        let alertController = UIAlertController(
+//            title: "광고 추적 권한 요청",
+//            message: "이 앱은 광고 추적을 사용하여 광고를 타겟팅하고 개인 정보를 수집합니다. 광고 추적을 허용하시겠습니까?",
+//            preferredStyle: .alert
+//        )
+//
+//        let allowAction = UIAlertAction(title: "허용", style: .default) { _ in
+//            // 사용자가 허용을 선택한 경우 처리
+//            // 광고 타겟팅 또는 추적 기능을 활성화할 수 있습니다.
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                if #available(iOS 14, *) {
+//                    ATTrackingManager.requestTrackingAuthorization { status in
+//                        switch status {
+//                        case .authorized:           // 허용됨
+//                            print("Authorized")
+//                            let identifier = ASIdentifierManager.shared().advertisingIdentifier
+//                            let idfaString = identifier.uuidString
+//
+//                            print("IDFA: \(idfaString)")
+//                            print("IDFA = \(ASIdentifierManager.shared().advertisingIdentifier)")
+//                        case .denied:               // 거부됨
+//                            print("Denied")
+//                        case .notDetermined:        // 결정되지 않음
+//                            print("Not Determined")
+//                        case .restricted:           // 제한됨
+//                            print("Restricted")
+//                        @unknown default:           // 알려지지 않음
+//                            print("Unknow")
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        let denyAction = UIAlertAction(title: "거부", style: .destructive) { _ in
+//            // 사용자가 거부를 선택한 경우 처리
+//            // 광고 타겟팅 또는 추적 기능을 비활성화하거나 대체 로직을 수행할 수 있습니다.
+//        }
+//
+//        alertController.addAction(allowAction)
+//        alertController.addAction(denyAction)
+//
+//        // 다이얼로그를 표시합니다.
+//        DispatchQueue.main.async {
+//            self.present(alertController, animated: true, completion: nil)
+//        }
+//    }
 }
 
