e97783118df799022ccc408817078833d42e1afb
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-10-27 17:06:36 +0900

    [Fix] #239 - enum 값 string으로 변경

--- pophory-iOS/Global/Consts/WebViewURLList.swift
+++ pophory-iOS/Global/Consts/WebViewURLList.swift
@@ -15,29 +15,18 @@ enum WebViewURLList {
     case settingPrivacyPolicy
     case settingTerms
     
-    var url: URL {
+    var url: String {
         switch self {
         case .mypageBannerAd:
-            return stringToURL(urlString: "https://www.figma.com/file/giJI7XdVoEcDMVDEeaStLe/3%EC%B0%A8-%EC%8A%A4%ED%94%84%EB%A6%B0%ED%8A%B8?type=design&node-id=1401-17024&mode=design&t=x5WBazl8fQ68va0R-0")
+            return "https://walla.my/pophory_event3?utm_source=ios"
         case .mypagePophoryStory:
-            return stringToURL(urlString: "https://pophoryofficial.wixsite.com/pophory/porit-story")
+            return "https://pophoryofficial.wixsite.com/pophory/porit-story"
         case .settingNotice:
-            return stringToURL(urlString: "https://pophoryofficial.wixsite.com/pophory/notice")
+            return "https://pophoryofficial.wixsite.com/pophory/notice"
         case .settingPrivacyPolicy:
-            return stringToURL(urlString: "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy2")
+            return "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy2"
         case .settingTerms:
-            return stringToURL(urlString: "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy1")
+            return "https://pophoryofficial.wixsite.com/pophory/%EC%A0%95%EC%B1%85#policy1"
         }
-    }
-}
-
-extension WebViewURLList {
-    private func stringToURL(urlString: String) -> URL {
-        if let url = URL(string: urlString) {
-            let request = URLRequest(url: url)
-        } else {
-            print("Invalid URL")
-        }
-        return url
     }
 }
