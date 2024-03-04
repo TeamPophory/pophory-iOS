450989f39d8eb0e3f51200501efa9e2678737c36
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-03-04 23:33:35 +0900

    [Fix] #270 - CFBundleShortVersionString 수정 및 스크립트 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -14,15 +14,17 @@ IFS='-' read -ra PARTS <<< "$CI_XCODE_SCHEME"
 
 # *.xconfig 파일 이름
 CONFIG_FILENAME="${PARTS[1]}.xcconfig"
+SHARED_CONFIG_FILENAME="Shared.xcconfig"
 
 # *.xconfig 파일의 전체 경로 계산
 CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
+SHARED_CONFIG_FILE_PATH="$FOLDER_PATH/$SHARED_CONFIG_FILENAME"
 
 # 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
 echo "BASE_URL = $BASE_URL" >> "$CONFIG_FILE_PATH"
 echo "UNIT_AD_ID = $UNIT_AD_ID" >> "$CONFIG_FILE_PATH"
-echo "SENTRY_DSN = $SENTRY_DSN" >> "$CONFIG_FILE_PATH"
-echo "ADMOB_APP_ID = $ADMOB_APP_ID" >> "$CONFIG_FILE_PATH"
+echo "SENTRY_DSN = $SENTRY_DSN" >> "$SHARED_CONFIG_FILE_PATH"
+echo "ADMOB_APP_ID = $ADMOB_APP_ID" >> "$SHARED_CONFIG_FILE_PATH"
 
 # 생성된 *.xconfig 파일 내용 출력
 cat "$CONFIG_FILE_PATH"
