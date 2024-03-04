8a8db9e51c725a0861c4e03639e4a34fb4255c69
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-03-04 23:19:59 +0900

    [Fix] #270 - scripts 파일 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,7 @@
 #
 
 # *.xconfig 파일이 생성될 폴더 경로
-FOLDER_PATH="/Volumes/workspace/repository/pophory-iOS/pophory-iOS"
+FOLDER_PATH="/Volumes/workspace/repository/pophory-iOS"
 
 # PARTS 배열의 두 번째 요소가 "Scheme Name"에 해당
 IFS='-' read -ra PARTS <<< "$CI_XCODE_SCHEME"
@@ -27,4 +27,4 @@ echo "ADMOB_APP_ID = $ADMOB_APP_ID" >> "$CONFIG_FILE_PATH"
 # 생성된 *.xconfig 파일 내용 출력
 cat "$CONFIG_FILE_PATH"
 
-echo "${PARTS[1]}.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다.
+echo "${PARTS[1]}.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다."
